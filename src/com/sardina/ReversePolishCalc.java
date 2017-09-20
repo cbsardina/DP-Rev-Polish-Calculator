a937f6f5941614ca4952b487356ec31012803da7
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-19 20:44:53 -0400

    DP-PolCalc: added bool to check for operators and math fn's.

--- src/com/sardina/ReversePolishCalc.java
+++ src/com/sardina/ReversePolishCalc.java
@@ -1,22 +1,26 @@
 package com.sardina;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class ReversePolishCalc {
 
     // You'll need a variable here to keep track of the top of the stack
-
-    // The array of the input string split up
-    private String[] tokens;
-
-    // The stack
-    private String[] stack;
+    Stack<String> tokens = new Stack<>();     // private String[] tokens; =>The array of the input string split up
+    private String[] stack;     // The stack
 
     public double calculate(String input) {
 
         // 1. Use the String split method to split the string into tokens at the commas
-        tokens = ...
+//        tokens = ...
+
+        for (String character : input.split(",")) {
+            tokens.add(character);
+        }
 
         // 2. Allocate a stack as big as the number of tokens
-        stack = ...
+        stack = tokens.toArray(new String[tokens.size()]);
 
         // 3. write the algorithm
         for(int i = 0; i < tokens.length; ++i) {
@@ -27,6 +31,32 @@ public class ReversePolishCalc {
         return pop();
     }
 
+//CHECK FOR OPERATOR
+    private boolean isOperator(String input) {
+        String[] operators = {"+", "-", "/", "*"};
+        for(String operator : operators) {
+            if(input.equals(operator)) {
+                return true;
+            } }return false; }
+
+//MATH FN's
+
+    private Double addFn(String input1, String input2) {
+        return Double.parseDouble(input1) + Double.parseDouble(input2);
+    }
+
+    private Double subtractFn(String input1, String input2) {
+        return Double.parseDouble(input1) - Double.parseDouble(input2);
+    }
+
+    private Double multiplyFn(String input1, String input2) {
+        return Double.parseDouble(input1) * Double.parseDouble(input2);
+    }
+    private Double divideFn(String input1, String input2) {
+        return Double.parseDouble(input1) / Double.parseDouble(input2);
+    }
+
+//ORIGINAL METHODS FROM STARTER FILES
     private void push(String number) {
         // push on the stack
     }
@@ -37,5 +67,10 @@ public class ReversePolishCalc {
 
     private double pop() {
         // remove the string from the top of the stack and convert it to a double and return it
+        return 4.4; //JUST TO RUN OTHER CODE REMOVE
     }
+
+
+
+
 }
